09aeb691b410418d1431d57bd96197ac238d68e6
Moses Musyimi <musyimi13@gmail.com>
2021-06-19 18:17:58 +0300

    client portal update

--- src/pages/ClientPortal.js
+++ src/pages/ClientPortal.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 function ClientPortal() {
   useEffect(() => {
-    window.location.href = "https://client.phpradius.com/login.php?i=6773210";
+    window.location.href = "https://client.phpradius.com/login.php?i=6773210/";
   }, []);
 
   return <div></div>;
