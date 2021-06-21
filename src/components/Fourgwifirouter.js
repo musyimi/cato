3497b2a7c800e71e561c9c5403837f8e95dfdec0
Moses Musyimi <musyimi13@gmail.com>
2021-06-21 13:58:47 +0300

    content for lte

--- src/components/Fourgwifirouter.js
+++ src/components/Fourgwifirouter.js
@@ -11,15 +11,39 @@ const Fourgwifirouter = () => {
         <div>
           <img className="h-96 object-cover w-full" src={Fiveg} alt="smart" />
         </div>
-        <div className="mt-16 p-4 space-y-2 ml-10">
+        <div className="mt-20 p-4 space-y-2 ml-10">
           <p className="text-5xl font-bold">5G/Private LTE</p>
 
-          <p>
-            5G will provide the speed, low latency and connectivity to enable a
-            new generation of applications, services and business opportunities
-            that have not been seen before
+          <p className="text-center">
+            LTE and 5G Wireless Edge Solutions for every network. To get
+            started, choose your experience...
           </p>
         </div>
+      </div>
+      <div className="text-center my-10">
+        <p className="text-3xl font-light">Private LTE</p>
+      </div>
+      <div className="mx-10">
+        <p className="text-center mt-10">
+          In large areas where organizations have essential connected devices
+          and applications, wireless connectivity is a challenge. Wi-Fi isn’t
+          secure enough and doesn’t scale well, but public LTE might not provide
+          the dedicated bandwidth or cost-effectiveness you need. Private
+          Cellular Networks, including Private LTE (PLTE), have emerged as the
+          best option for wireless networking in sprawling spaces ranging from
+          campuses, shipping ports, and refineries to Smart Cities. In these
+          “wide-area LAN” scenarios, Private LTE combines the control and fixed
+          costs of a private network with the flexibility, security, and
+          macro-network benefits of cellular broadband — with a built-in pathway
+          to Private 5G.
+        </p>
+      </div>
+      <div className="mx-10">
+        <p className=" mt-4">
+          Private LTE for Enterprise and Public Sector, School districts,
+          Shipping ports, Smart Cities, Oil and gas, Manufacturing, Distribution
+          centers, Mines, Business, college campuses, Stadiums and Hospitals
+        </p>
       </div>
       <div>
         <Bottom />
