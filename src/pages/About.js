c7e91b492a1ef4e2656b7d3d54c2b0b247eb3f67
Moses Musyimi <musyimi13@gmail.com>
2021-06-28 19:23:12 +0300

    opacity of the top image adjustment

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,10 +8,14 @@ const About = () => {
   return (
     <div className="relative">
       <div>
-        <img className=" h-96 w-full object-cover" src={Us} alt="about us" />
+        <img
+          className=" h-96 w-full object-cover opacity-40"
+          src={Us}
+          alt="about us"
+        />
       </div>
       <div>
-        <p className="xs:p-2 absolute lg1:top-24 lg1:text-center lg1:text-3xl lg1:font-thin lg1:mx-10 text-gray-50">
+        <p className="xs:p-2 absolute lg1:top-24 lg1:text-center lg1:text-3xl lg1:font-semibold lg1:mx-10 text-gray-900">
           We are a pioneer digital champion, always been focused on innovation
           and evolution, thinking about future to make it, to stay ahead as a
           truly meaningful and purposeful organization. We offer variety of ICT
